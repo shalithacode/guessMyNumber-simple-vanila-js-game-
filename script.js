692ce0c5469b0c3d8586732e568a46faca7b5d96
shalithacode <shalithalakruwan1998@gmail.com>
2021-02-16 00:39:05 +0530

    Complete implementation

--- script.js
+++ script.js
@@ -9,37 +9,60 @@ const hidden = document.querySelector('.number');
 const checkBtn = document.querySelector('.check');
 const againBtn = document.querySelector('.again');
 
-//gameLogic
-let x = Math.trunc(Math.random()*20)+1;
+const initialColor = document.body.style.backgroundColor;
 
-const checkNumber = function(){
+var x = Math.trunc(Math.random() * 20) + 1;
+var i = 20;
+
+//All reset
+const reset = function () {
+    x = Math.trunc(Math.random() * 20) + 1;
+    message.textContent = "Start guessing...";
+    i = 20;
+    document.body.style.backgroundColor = initialColor;
+    score.textContent = `💯 Score: 20`;
+    hidden.textContent = '?';
+    checkBtn.disabled = false;
+}
+//gameLogic
+
+const checkNumber = function () {
     const guessNo = Number(input.value);
 
-    
-    if(!guessNo){
+    if (!guessNo) {
         message.textContent = "No number!";
-    }else{
-        if(guessNo>20 || guessNo<1) 
-        {
+    } else {
+        if (guessNo > 20 || guessNo < 1) {
+            countDown()
+            score.textContent = `💯 Score: ${i}`;
             message.textContent = "Invalid number!";
 
         }
-        else if(guessNo === x){
+        else if (guessNo === x) {
             message.textContent = "Correct Number!";
             hidden.textContent = x;
+            highscore.textContent = `🥇 Highscore: ${i}`;
+            document.body.style.backgroundColor = "Green";
+            checkBtn.disabled = true;
 
-        }else{
-            if(x > guessNo) message.textContent = "Too high...";
+        } else {
+            countDown()
+            if (x > guessNo) message.textContent = "Too high...";
             else message.textContent = "Too low...";
         }
     }
+    function countDown() {
+        i--;
+        if (i === 0) {
+            alert("Game Over!");
+            window.location.reload();
+        }
+        score.textContent = `💯 Score: ${i}`;
+    }
+
 }
 
-const reset = function(){
-    let x = Math.trunc(Math.random*20)+1;
-    message.textContent = "Start guessing...";
 
-}
-
+//Button listener
 checkBtn.addEventListener('click', checkNumber);
 againBtn.addEventListener('click', reset);

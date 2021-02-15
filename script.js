7aa670b2105dadb50237454bd63ca88f76f59654
shalithacode <shalithalakruwan1998@gmail.com>
2021-02-15 15:32:35 +0530

    game logic added

--- script.js
+++ script.js
@@ -9,3 +9,37 @@ const hidden = document.querySelector('.number');
 const checkBtn = document.querySelector('.check');
 const againBtn = document.querySelector('.again');
 
+//gameLogic
+let x = Math.trunc(Math.random()*20)+1;
+
+const checkNumber = function(){
+    const guessNo = Number(input.value);
+
+    
+    if(!guessNo){
+        message.textContent = "No number!";
+    }else{
+        if(guessNo>20 || guessNo<1) 
+        {
+            message.textContent = "Invalid number!";
+
+        }
+        else if(guessNo === x){
+            message.textContent = "Correct Number!";
+            hidden.textContent = x;
+
+        }else{
+            if(x > guessNo) message.textContent = "Too high...";
+            else message.textContent = "Too low...";
+        }
+    }
+}
+
+const reset = function(){
+    let x = Math.trunc(Math.random*20)+1;
+    message.textContent = "Start guessing...";
+
+}
+
+checkBtn.addEventListener('click', checkNumber);
+againBtn.addEventListener('click', reset);

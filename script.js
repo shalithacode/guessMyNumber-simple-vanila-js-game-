32318ec14a37402c4834e24ca7790722ee318a71
shalithacode <shalithalakruwan1998@gmail.com>
2021-02-15 14:39:04 +0530

    referances added

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+//referenaces
+const message = document.querySelector('.message');
+const score = document.querySelector('.label-score');
+const highscore = document.querySelector('.label-highscore');
+const input = document.querySelector('.guess');
+const hidden = document.querySelector('.number');
+const checkBtn = document.querySelector('.check');
+const againBtn = document.querySelector('.again');
+
